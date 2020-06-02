0855a88919b3bdf9ae2064bc39303ceeb1dba693
Alexey Alexeev <alex.alexeev23061991@gmail.com>
2020-06-02 11:13:51 +0300

    add icons

---
+++ src/components/Flexbox/index.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const Flexbox = styled.div`
+  display: flex;
+  justify-content: ${(props) => props.justify};
+  flex-direction: ${(props) => props.direction};
+  align-items: ${(props) => props.align};
+  margin: ${(props) => props.margin};
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  text-align: ${(props) => props.textAlign};
+  cursor: ${(props) => props.cursor};
+  flex-wrap: ${(props) => props.flexWrap};
+  z-index: ${(props) => props.zIndex};
+  position: ${(props) => props.position || 'unset'};
+  padding: ${(props) => props.padding};
+`;
+
+export default { Flexbox };
