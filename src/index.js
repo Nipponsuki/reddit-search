c017bd2577949cc142d577bd01d3da6d7193b502
Alexey Alexeev <alex.alexeev23061991@gmail.com>
2020-06-03 11:16:18 +0300

    add local storage persistance

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from 'store/store';
+import { store, persistor } from 'store/store';
 import GlobalStyle from 'components/GlobalStyles/GlobalStyles';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <GlobalStyle />
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <GlobalStyle />
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import undoable from 'redux-undo';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import bookmarksReducer from './bookmarksStore';
 import searchReducer from './searchStore';
 
-export default configureStore({
-  reducer: {
-    search: searchReducer,
-    bookmarks: undoable(bookmarksReducer),
-  },
+const reducers = combineReducers({
+  search: searchReducer,
+  bookmarks: undoable(bookmarksReducer),
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['bookmarks'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
